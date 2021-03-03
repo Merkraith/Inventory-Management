7e75f4408f4a8da4db05f8bd77174db53df1b7b3
Mark Heil <mark.heil1122@gmail.com>
2021-03-03 00:50:45 -0800

    component is now mapping different products

--- src/components/StockEventsTable.js
+++ src/components/StockEventsTable.js
@@ -1,17 +1,28 @@
 import React from 'react'
 
 function StockEventsTable(props) {
-    const { stockEvents } = props
+    const { products, stockEvents } = props
+
     return (
         <div className="StockEventTable">
-            {stockEvents.map(event => (
-                <div>
-                    <p>Id: {event.id}</p>
-                    <p>Type: {event.type}</p>
-                    <p>Quantity: {event.qty}</p>
-                    <p>Product Name: {event.product.name}</p>
-                </div>
-            ))}
+            {products.map(product => {
+                const { id } = product
+
+                const releventStockEvents = stockEvents.filter(se => se.product.id === product.id)
+                return (
+                    <div className="StockEventTable__ProductsContainer">
+                        <h2>Product: {product.name}</h2>
+                        {releventStockEvents.map(event => (
+                            <div className="StockEventTable__Card">
+                                <p>Id: {event.id}</p>
+                                <p>Type: {event.type}</p>
+                                <p>Quantity: {event.qty}</p>
+                                <p>Product Name: {event.product.name}</p>
+                            </div>
+                        ))}
+                    </div>
+                )
+            })}
         </div>
     )
 }
